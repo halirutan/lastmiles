38f1dd5086306aa033809a2dfae41b107bb6b6ad
blastwave <dclarke@blastwave.org>
2019-09-14 01:47:26 +0000

    20190914014726 : test maxalloc with bytes and options for malloc delta

--- mem_hack/maxalloc.c
+++ mem_hack/maxalloc.c
@@ -19,9 +19,10 @@
 
 int main( int argc, char *argv[] ) {
 
-    int max = -1;
+    size_t max = 0;
     int mallop = 0;
-    int j, mb;
+    int j;
+    size_t num_bytes, mem_alloc_size, mem_delta_size;
     int bytes_formatted;
     char* buffer;
     struct timespec tn;
@@ -39,17 +40,25 @@ int main( int argc, char *argv[] ) {
         }
     }
 
-    if ( argc == 2 ) {
-        max = atoi(argv[1]);
-        if ( max == 0 )
-            printf ( " NOTE : will try to allocate all available memory.\n" );
-        else
-            printf ( " NOTE : will try to allocate %5i MB of memory.\n", max );
+    if ( argc == 4 ) {
+        max = (size_t)strtol(argv[1], (char **)NULL, 10);
+        mem_alloc_size = (size_t)strtol(argv[2], (char **)NULL, 10);
+        mem_delta_size = (size_t)strtol(argv[3], (char **)NULL, 10);
+
+        if ( max < (size_t)2147483648 ) {
+            fprintf(stderr, "FAIL : lets allocation some reasonable amount!\n");
+            return ( EXIT_FAILURE );
+        }
+
+        printf ( " NOTE : will try to allocate %5i bytes of memory.\n", max );
+        printf ( "      : we will use an init allocation of %i bytes.\n", mem_alloc_size );
+        printf ( "      : with a delta memory allocation of %i bytes.\n", mem_delta_size );
 
     } else {
 
-        printf ( "Will try to allocate all available memory.\n" );
-        printf ( "This MAY be the last thing your system does.\n" );
+        fprintf (stderr, "FAIL : enter memsize total and allocation\n" );
+        fprintf (stderr, "     : init size and allocation delta.\n" );
+        return ( EXIT_FAILURE );
 
     }
 
@@ -57,21 +66,21 @@ int main( int argc, char *argv[] ) {
         printf ( "WARNING : You must at least use 8 MB of memory!\n" );
         printf ( "        : quitting .. no harm done.\n" );
     } else {
-        mb = 8;
-        while (((buffer=malloc((size_t)mb*(size_t)1048576)) != NULL ) 
+        num_bytes = mem_alloc_size;
+        while (((buffer=malloc(num_bytes)) != NULL ) 
                              && 
-               (( mb<max ) || ( max==0 )) )
+               (( num_bytes<max ) || ( max==0 )) )
         {
             /* flip bits of all memory in the buffer */
-            memset( buffer, 0x00, (size_t) mb*1024*1024 );
-            memset( buffer, 0xaa, (size_t) mb*1024*1024 );
-            memset( buffer, 0x55, (size_t) mb*1024*1024 );
-            memset( buffer, 0xff, (size_t) mb*1024*1024 );
+            memset( buffer, 0x00, num_bytes );
+            memset( buffer, 0xaa, num_bytes );
+            memset( buffer, 0x55, num_bytes );
+            memset( buffer, 0xff, num_bytes );
 
-            mb+=8;
+            num_bytes+=mem_delta_size;
 
-            if ( ( mb % 16 ) == 0 ) {
-                printf( "      : Allocated %6i MB", mb);
+            if ( ( num_bytes % ( 8 * 1048576 ) ) == 0 ) {
+                printf( "      : Allocated %6i MB", (num_bytes/1048576) );
 
                 /* Get the REALTIME_CLOCK time */
                 if ( clock_gettime( CLOCK_REALTIME, &tn ) > -1 ) {
@@ -91,12 +100,12 @@ int main( int argc, char *argv[] ) {
 
         }
 
-        if ( ( buffer == NULL ) && ( ( mb < max ) || ( max == 0 ) )  ) { 
+        if ( ( buffer == NULL ) && ( ( num_bytes < max ) || ( max == 0 ) )  ) { 
             printf ( " NOTE : malloc returned NULL.\n" );
             printf ( "      : Most likely there is no more free memory.\n" );
         }
 
-        printf( "FINAL : Allocated %d MB\n", mb);
+        printf( "FINAL : Allocated %d bytes\n", num_bytes);
 
     }
 
