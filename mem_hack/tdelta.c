e19f3bc8e18a33beb4807b63389c90b9ede7c32e
blastwave <dclarke@blastwave.org>
2019-09-14 03:01:27 +0000

    20190914030127 : test tdelta with clock_gettime on FreeBSD 12 amd64

--- mem_hack/tdelta.c
+++ mem_hack/tdelta.c
@@ -1,4 +1,17 @@
 
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+ *********************************************************************/
+#define _XOPEN_SOURCE 600
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
