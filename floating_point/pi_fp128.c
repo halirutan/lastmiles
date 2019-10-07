32c1a69cde6f1cdc8cfea148ee6b1d58542c509c
blastwave <dclarke@blastwave.org>
2019-10-07 21:11:45 +0000

    20191007211145 : quick demonstration of a tenth addition loss in precision

--- floating_point/pi_fp128.c
+++ floating_point/pi_fp128.c
@@ -7,7 +7,49 @@ int main(int argc, char *argv[])
 
     long double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280L;
 
-    printf("pi could be %42.36Le\n", pi);
+    printf("the sizeof(pi) is %i bytes\n", sizeof(long double) );
+    printf("pi could be %42.38Le\n", pi);
+
+
+    long double tenth = 0.1L;
+
+
+    long double sum = 0.0L;
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum + tenth;
+    printf("sum = %42.38Le\n", sum);
+
+    sum = sum - 1.0L;
+
+    printf("final sum is %42.38Le\n", sum);
+
     return ( EXIT_SUCCESS );
 
 }
